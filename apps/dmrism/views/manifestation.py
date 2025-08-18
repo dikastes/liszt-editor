b3a48d6a4e3558f7f45d76a8f08f81114d656042
Matthias Richter <matthias.richter@slub-dresden.de>
2025-08-18 17:12:06 +0200

    Add new views

--- apps/dmrism/views/manifestation.py
+++ apps/dmrism/views/manifestation.py
@@ -1,14 +1,15 @@
-from django.views.generic import ListView
+from ..forms import ManifestationForm
+from ..models.manifestation import Manifestation
+from copy import deepcopy as clone
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
-from django.views.generic import DeleteView, DetailView
+from django.views.generic import ListView, DeleteView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
-from ..models.manifestation import Manifestation
-from liszt_util.views.base import *
-from ..forms import ManifestationForm
+from haystack.generic_views import SearchView
+from liszt_util.forms import SearchForm
 
 
-class ManifestationListView(AbstractListView):
+class ManifestationListView(ListView):
     template_name = 'dmrism/list.html'
     model = Manifestation
     paginate_by = 10
@@ -19,7 +20,7 @@ class ManifestationListView(AbstractListView):
         return context
 
 
-class ManifestationSearchView(AbstractSearchView):
+class ManifestationSearchView(SearchView):
     template_name = 'dmrism/list.html'
     model = Manifestation
     paginate_by = 10
@@ -29,7 +30,7 @@ class ManifestationSearchView(AbstractSearchView):
 
         # redirect to list view if empty query
         if not query or query == '':
-            return redirect(f'dmrism:{camel_to_snake_case(self.get_model_name())}_list')
+            return redirect(f'dmrism:manifestation_list')
 
         return super().get(request, *args, **kwargs)
 
@@ -48,47 +49,22 @@ class ManifestationCreateView(CreateView):
         self.object.pull_rism_data()
         return response
 
-"""
-old edwoca create view
-    def get_success_url(self):
-        return self.object.get_absolute_url()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if 'title_formset' not in context:
-            if self.request.POST:
-                context['title_formset'] = ManifestationTitleFormSet(self.request.POST, self.request.FILES)
-            else:
-                ManifestationTitleFormSet.can_delete = False
-                context['title_form_set'] = ManifestationTitleFormSet()
-        return context
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        title_formset = ManifestationTitleFormSet(self.request.POST, self.request.FILES, instance=self.object)
-
-        if title_formset.is_valid():
-            self.object.save()
-            title_formset.save()
-            return redirect(self.get_success_url())
-        else:
-            self.object = None
-            return self.form_invalid(form, title_formset=title_formset)
-
-    def form_invalid(self, form, title_formset=None):
-        context = self.get_context_data(form=form, title_formset=title_formset)
-        return self.render_to_response(context)
-"""
-
 
 class ManifestationDetailView(DetailView):
     model = Manifestation
+    template_name = 'dmrism/detail.html'
 
 
 class ManifestationUpdateView(UpdateView):
     model = Manifestation
     form_class = ManifestationForm
-    template_name = 'edwoca/manifestation_update.html'
+    template_name = 'dmrism/form.html'
+
+    def form_valid(self, form):
+        old_manifestation = Manifestation.objects.get(id = self.object.id)
+        if old_manifestation.rism_id != self.object.rism_id:
+            self.object.rism_id_unaligned = True
+        return super().form_valid(form)
 
 
 class ManifestationDeleteView(DeleteView):
@@ -98,10 +74,53 @@ class ManifestationDeleteView(DeleteView):
 
 
 def manifestation_pull(request, pk):
-    pass
+    existing_temporary_manifestation = Manifestation.objects.filter(temporary_target = pk).first()
+    if existing_temporary_manifestation:
+        existing_temporary_manifestation.delete()
+
+    manifestation = Manifestation.objects.get(id = pk)
+    temporary_manifestation = Manifestation.objects.create(rism_id = manifestation.rism_id)
+    temporary_manifestation.pull_rism_data()
+    temporary_manifestation.temporary = True
+    temporary_manifestation.temporary_target = manifestation
+    temporary_manifestation.save()
+
+    # do we need the temp flag on items as well?
+    context = {}
+    context['object'] = manifestation
+    context['compare_object'] = temporary_manifestation
+
+    return render(request, 'dmrism/confirm_pull.html', context)
+
 
 def manifestation_confirm_pull(request, pk):
-    pass
+    temporary_manifestation = Manifestation.objects.get(temporary_target = pk)
+    manifestation = Manifestation.objects.get(id = pk)
+
+    manifestation.items.all().delete()
+    for item in temporary_manifestation.items.all():
+        manifestation.items.add(item)
+
+    manifestation.related_manifestations.all().delete()
+    for manifestation_relation in temporary_manifestation.related_manifestations.all():
+        manifestation.related_manifestations.add(manifestation_relation)
+
+    manifestation.manifestation_form = temporary_manifestation.manifestation_form
+    manifestation.dedication = temporary_manifestation.dedication
+    manifestation.handwriting = temporary_manifestation.handwriting
+    manifestation.extent = temporary_manifestation.extent
+    manifestation.paper = temporary_manifestation.paper
+    manifestation.private_comment = temporary_manifestation.private_comment
+
+    manifestation.save()
+    temporary_manifestation.delete()
+
+    return redirect('dmrism:manifestation_detail', pk = pk)
+
 
 def manifestation_reject_pull(request, pk):
-    pass
+    temporary_manifestation = Manifestation.objects.get(temporary_target = pk)
+    manifestation = Manifestation.objects.get(id = pk)
+    temporary_manifestation.delete()
+
+    return redirect('dmrism:manifestation_detail', pk = pk)
