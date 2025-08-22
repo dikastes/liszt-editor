e06aa40e2c14f293416e4738bfaa15f5f4ca0a47
Matthias Richter <matthias.richter@slub-dresden.de>
2025-08-22 17:50:34 +0200

    Cleanup: Minor adjustments to item forms and views
    This commit includes minor cleanups and adjustments to item-related forms and views.
    
    - Comments out unused `ItemForm` in `apps/edwoca/forms/item.py`.
    - Adjusts `manifestation_update` in `apps/edwoca/views/manifestation.py` to correctly use `ItemForm` when creating new items.

--- apps/edwoca/forms/item.py
+++ apps/edwoca/forms/item.py
@@ -7,25 +7,23 @@ from django.forms.models import inlineformset_factory
 from django.utils.safestring import mark_safe
 
 
-"""
 class ItemForm(ModelForm):
     class Meta:
         model = Item
-        fields = ['rism_id']
-        widgets = {
-                'rism_id': TextInput( attrs = {
-                        'class': 'grow h-full'
-                    })
-            }
+        fields = []
+        #widgets = {
+                #'rism_id': TextInput( attrs = {
+                        #'class': 'grow h-full'
+                    #})
+            #}
 
-    def as_daisy(self):
-        form = div()
-        rism_id_label = label(self['rism_id'].label, _for=self['rism_id'].id_for_label, cls='input input-bordered flex items-center gap-2 my-5')
-        rism_id_label.add(raw(str(self['rism_id'])))
-        form.add(rism_id_label)
+    #def as_daisy(self):
+        #form = div()
+        #rism_id_label = label(self['rism_id'].label, _for=self['rism_id'].id_for_label, cls='input input-bordered flex items-center gap-2 my-5')
+        #rism_id_label.add(raw(str(self['rism_id'])))
+        #form.add(rism_id_label)
 
-        return mark_safe(str(form))
-"""
+        #return mark_safe(str(form))
 
 
 class SignatureForm(ModelForm):
