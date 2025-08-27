0d1bdc218212e62b554069b3f274b2aeeea7eb8f
Nathanael Mehlhorn <nathanael.georg.mehlhorn@slub-dresden.de>
2025-08-27 12:29:20 +0200

    Layouts, daisy_UI mixin

--- apps/liszt_util/forms/forms.py
+++ apps/liszt_util/forms/forms.py
@@ -1,8 +1,22 @@
 from .layouts import Layouts
+from django.utils.safestring import mark_safe
 
 class GenericAsDaisyMixin():
 
-    layout = Layouts.LABEL_OUTSIDE
+    # Default layout, may be overwritten by subclass
+    layout : Layouts = Layouts.LABEL_INSIDE
 
     def as_daisy(self):
+        match self.layout:
+            case Layouts.CUSTOM:
+                raise NotImplementedError("Please override as_daisy or choose a template layout")
+            case Layouts.LABEL_INSIDE:
+                return mark_safe(self._inside_layout())
+            case Layouts.LABEL_OUTSIDE:
+                return mark_safe(self._outside_layout())
+    
+    def _inside_layout(self):
+        pass
+    
+    def _outside_layout(self):
         pass
--- apps/liszt_util/forms/layouts.py
+++ apps/liszt_util/forms/layouts.py
@@ -3,3 +3,4 @@ from enum import Enum
 class Layouts(Enum):
     LABEL_INSIDE = 'label_inside'
     LABEL_OUTSIDE = 'label_outside'
+    CUSTOM = 'layout_custom'
