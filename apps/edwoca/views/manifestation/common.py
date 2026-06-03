79b48218552ad2129a4d44c23341db766017692e
Nathanael Georg Mehlhorn <nathanael.georg.mehlhorn@slub-dresden.de>
2026-06-03 17:35:02 +0200

    Implemented nicer publisher search field

--- apps/edwoca/views/manifestation/common.py
+++ apps/edwoca/views/manifestation/common.py
@@ -1,6 +1,8 @@
 import re
 
-from haystack.query import SQ
+from haystack.query import SearchQuerySet
+
+from dmad_on_django.models import Corporation
 from ...forms.manifestation import *
 from calendar import monthrange
 from ...forms.item import SignatureForm, ItemDigitizedCopyForm, PersonProvenanceStationForm, CorporationProvenanceStationForm, ItemProvenanceCommentForm, NewItemSignatureFormSet, ItemManuscriptForm, ItemHandwritingForm, PersonProvenanceStationBibForm, CorporationProvenanceStationBibForm, PersonProvenanceFormSet, PersonProvenanceBibFormSet, CorporationProvenanceFormSet, CorporationProvenanceBibFormSet, PersonProvenanceStationWebReference, CorporationProvenanceStationWebReference, PersonProvenanceStationWebReferenceForm, CorporationProvenanceStationWebReferenceForm
@@ -11,9 +13,9 @@ from ...models import Manifestation as EdwocaManifestation, Letter, Expression, 
 from ..base import *
 from ...models import ManifestationTitle, ManifestationTitleHandwriting, ItemDigitalCopy
 from bib.models import ZotItem
-from django.db.models import Q
+from django.db.models import Q, Model
 from django.forms import inlineformset_factory
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.utils.translation import gettext_lazy as _
@@ -208,6 +210,7 @@ def manifestation_create(request, publisher_pk=None):
 
             chosen_publisher = form.cleaned_data.get('publisher')
             if chosen_publisher:
+                chosen_publisher = Corporation.objects.get(pk=chosen_publisher)
                 Publication.objects.create(
                         publisher = chosen_publisher,
                         manifestation = manifestation
@@ -215,28 +218,22 @@ def manifestation_create(request, publisher_pk=None):
 
             return redirect('edwoca:manifestation_update', pk=manifestation.pk)
     else:
-        if publisher:
-            form = ManifestationCreateForm(initial = {'publisher': publisher})
-
-        else:
-            form = ManifestationCreateForm()
 
         context = {
-            'form': form,
+            'form': ManifestationCreateForm(),
             'referrer': 'manifestation_create'
         }
 
+        return render(request, 'edwoca/create_manifestation.html', context)
 
-    if not publisher:
-        if request.GET.get('q'):
-            search_form = SearchForm(request.GET)
-            context['search_form'] = search_form
-            context['publisher_list'] = search_form.search().models(Corporation)
-        else:
-            context['search_form'] = SearchForm()
+def publisher_search_view(request):
+    search_text = request.GET.get('publisher_search', '')
 
-    return render(request, 'edwoca/create_manifestation.html', context)
+    if len(search_text) < 2:
+        return HttpResponse('')
 
+    publisher = SearchQuerySet().models(Corporation).filter(content=search_text)
+    return render(request, 'edwoca/partials/manifestation/publisher_results.html', {'publisher_list': publisher})
 
 def manifestation_update(request, pk):
     manifestation = Manifestation.objects.get(id=pk)
