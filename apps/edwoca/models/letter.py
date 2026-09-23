9de179cb170273175cf25bcbfcf4552d003db502
Matthias Richter <matthias.richter@slub-dresden.de>
2026-09-23 17:33:44 +0200

    Prepare roll out

--- apps/edwoca/models/letter.py
+++ apps/edwoca/models/letter.py
@@ -160,7 +160,7 @@ class Letter(TrackedModel):
     objects = DisplayableQuerySet.as_manager()
 
     ordering_fields = {
-        'sort_edition': (['edition'], _('edition'), _('A-Z'), _('Z-A'))
+        'sort_edition': (['sort_edition'], _('letter edition'), _('A-Z'), _('Z-A'))
     }
 
     @classmethod
